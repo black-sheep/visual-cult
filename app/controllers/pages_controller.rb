131bdc61710942e115e879ffe505d939749e4a07
Piterskiy <piterskiy.b@gmail.com>
2012-11-08 17:15:26 +0400

    first v6

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
+  before_filter :im_admin?, only: [ :admin ]
+
   def index
-    #reset_session
   end
 
   def admin
 
+  end
+
+  private
+  def im_admin?
+    redirect_to root_path unless current_user && current_user.roles.exists?(Role.find(1))
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def user_avatar(user)
+    if File.exists?(user.avatar.url.to_s)
+      user.avatar.url
+    else
+      'placeholder/placeholder.jpg'
+    end
+  end
 end
